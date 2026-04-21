52aed430bdb05da03a14baee97442ef036e47320
davis <davis.fei@bytedance.com>
2026-04-21 20:04:03 +0800

    chore: remove ios app support and capacitor dependencies

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.trae.cloudnotes',
-  appName: 'CloudNotes',
-  webDir: 'dist'
-};
-
-export default config;
